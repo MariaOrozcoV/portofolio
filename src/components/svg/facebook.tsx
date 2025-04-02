d7a405292c2858ae9554768f8322776932d02024
yersonargotev <yersonargotev@gmail.com>
2025-04-02 15:03:36 -0500

    Arreglando errores de tipos

--- src/components/svg/facebook.tsx
+++ src/components/svg/facebook.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import type React from "react";
 
-const Facebook = (props) => (
+const Facebook = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={24}
